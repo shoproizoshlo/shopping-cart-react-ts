8f72604e5f8b81841fba1e16546d0ab5142c023a
shoproizoshlo <iamsusunny@gmail.com>
2024-03-12 11:16:21 +0100

    created CartItemType

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 
+import { Wrapper } from "./App.styles";
+
 import "./App.css";
 
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType> =>
+  await (
+    await fetch("https://fakestoreapi.com/products")
+  ).json;
+
 function App() {
-  return <>piska</>;
+  return <>start</>;
 }
 
 export default App;
