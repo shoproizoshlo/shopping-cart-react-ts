4f2ffcb89de96b93e4febf65aace63f980b70c64
shoproizoshlo <iamsusunny@gmail.com>
2024-03-12 12:27:05 +0100

    created Cart component

---
+++ src/Components/CartItem/CartItem.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+import { CartItemType } from "../../App";
+
+import { Wrapper } from "./CartItem.styles";
+
+const CartItem: React.FC = () => <div>Cart item</div>;
+
+export default CartItem;
